63a4ced4f9c68342e8cdabe2ae193371f5b2c917
szaqal <malczyk.pawel@gmail.com>
2019-12-30 10:18:06 +0100

    istio

--- dummy-http/main.go
+++ dummy-http/main.go
@@ -5,21 +5,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 // TimeResponse returns current time
 type TimeResponse struct {
 	TimeValue string `json:"time"`
+	HostName  string `json:"host"`
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	hostName, _ := os.Hostname()
 	currTime := time.Now().Format(time.RFC3339)
 	content, _ := json.Marshal(&TimeResponse{
 		TimeValue: currTime,
+		HostName:  hostName,
 	})
 	w.Header().Add("Content-Type", "application/json")
-	fmt.Fprintf(w, "%s", content)
+	fmt.Fprintf(w, "%s\n", content)
 }
 
 func main() {
