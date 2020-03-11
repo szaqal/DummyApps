e254cdcbfa1fd43643a3902acf70b1ae95e8bde6
szaqal <malczyk.pawel@gmail.com>
2020-03-11 09:23:54 +0100

    Some work

--- dummy-grpc-testing/build.sh
+++ dummy-grpc-testing/build.sh
@@ -9,8 +9,14 @@ server=$(pwd)/server
 
 echo "Build Server"
 cd $server && go build
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o dummy-grpc-clock-server
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o dummy-grpc-testing-server
+docker build -t dummy-grpc-testing-server .
+docker tag dummy-grpc-testing-server szaqal/dummy-grpc-testing-server:latest
+docker push szaqal/dummy-grpc-testing-server:latest
 
 echo "Build Client"
 cd $client && go build
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o dummy-grpc-clock-client
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o dummy-grpc-testing-client
+docker build -t dummy-grpc-testing-client .
+docker tag dummy-grpc-testing-client szaqal/dummy-grpc-testing-client:latest
+docker push szaqal/dummy-grpc-testing-client:latest
