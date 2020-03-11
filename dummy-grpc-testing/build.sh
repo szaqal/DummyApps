ceaca8d997ad4565ab3a871762a6344dd34499f6
szaqal <malczyk.pawel@gmail.com>
2020-03-11 11:17:15 +0100

    Some work

--- dummy-grpc-testing/build.sh
+++ dummy-grpc-testing/build.sh
@@ -2,11 +2,10 @@
 
 echo "Proto (GO)"
 protoc -I service/ service/service.proto --go_out=plugins=grpc:service
-echo "Proto (JAVA)"
-protoc -I service/ --java_out=client_java/grpc-client/src/main/java service/service.proto
 
 
 client=$(pwd)/client_go
+client_java=$(pwd)/client_java
 server=$(pwd)/server
 
 echo "Build Server"
@@ -16,9 +15,13 @@ docker build -t dummy-grpc-testing-server .
 docker tag dummy-grpc-testing-server szaqal/dummy-grpc-testing-server:latest
 docker push szaqal/dummy-grpc-testing-server:latest
 
-echo "Build Client"
+echo "Build Client (GO)"
 cd $client && go build
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o dummy-grpc-testing-client
 docker build -t dummy-grpc-testing-client .
 docker tag dummy-grpc-testing-client szaqal/dummy-grpc-testing-client:latest
 docker push szaqal/dummy-grpc-testing-client:latest
+
+
+echo "Build Client (JAVA)"
+cd $client_java && mvn -f grpc-client/pom.xml clean install 
--- dummy-grpc-testing/client_java/grpc-client/src/main/java/com/dummy/grpc/App.java
+++ dummy-grpc-testing/client_java/grpc-client/src/main/java/com/dummy/grpc/App.java
@@ -4,6 +4,8 @@ package com.dummy.grpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
+import java.util.Optional;
+
 import com.dummy.grpc.DelayServiceGrpc.DelayServiceBlockingStub;
 import com.dummy.grpc.Service.ServiceResponse;
 import com.google.protobuf.Empty;
@@ -11,7 +13,8 @@ import com.google.protobuf.Empty;
 public class App {
 
   public static void main(String[] args) {
-    ManagedChannel channel = ManagedChannelBuilder.forTarget(System.getenv("SERVER_ADDRESS")).usePlaintext().build();
+    String serverAddress = System.getenv("SERVER_ADDRESS");
+    ManagedChannel channel = ManagedChannelBuilder.forTarget(Optional.ofNullable(serverAddress).orElse("localhost:50051")).usePlaintext().build();
     DelayServiceBlockingStub delayServiceBlockingStub = DelayServiceGrpc.newBlockingStub(channel);
     ServiceResponse perform = delayServiceBlockingStub.perform(Empty.newBuilder().build());
     System.out.println(perform.getMessage());
